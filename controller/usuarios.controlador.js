14b10aa914ab33147e78e0f0da659f00f4e720d5
efrain <cuter.efrain@gmail.com>
2021-05-11 14:51:02 -0400

    RESTSERVER-01

--- controller/usuarios.controlador.js
+++ controller/usuarios.controlador.js
@@ -1,5 +1,9 @@
 //Desestructuracion del response
 const { response } = require('express');
+//Importar el paquete para encriptacion
+const bcryptjs=require('bcryptjs');
+//Importar el modelo
+const Usuario = require('../models/usuario');
 
 const usuariosGet = (req, res = response) => {
     //res.send('SERVIDOR REST');
@@ -13,13 +17,19 @@ const usuariosPut = (req, res) => {
         mensaje: 'PUT API CONTROLADOR'
     });
 }
-const usuariosPost = (req, res) => {
-    //const body=req.body
-    const { nombre, ci } = req.body;
+const usuariosPost = async (req, res) => {
+    //Separar los campos que se van a enviar 
+    const {nombre,correo,password, rol} = req.body;
+    //Creamos la instancia 
+    const usuario = new Usuario({nombre,correo,password, rol});
+    //encriptar la contraseña, en la documentacion revisar un salt es el numero de vueltas
+    const salt=bcryptjs.genSaltSync();
+    usuario.password=bcryptjs.hashSync(password,salt);
+    //Grabar el registro
+    await usuario.save();
     res.json({
         mensaje: 'POST API CONTROLADOR',
-        nombre,
-        ci
+        usuario
     });
 }
 const usuariosDelete = (req, res) => {
--- models/servidor.js
+++ models/servidor.js
@@ -1,17 +1,25 @@
 
-const express=require('express');
-const cors=require('cors');
+const express = require('express');
+const cors = require('cors');
+//desestrucuturar para solo obtener la ConexionDB
+const { ConexionDB } = require('../database/config');
 
 class Servidor {
     //Creamos el constructor
-    constructor(){
-        this.app=express();
-        this.port=process.env.PORT;
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        //crear la fucion conectar al inciar la aplicacion
+        this.conectarDB();
         this.middlewares();
         this.routes();
     }
+    // crear la fuciona concetarDB
+    async conectarDB() {
+        ConexionDB();
+    }
 
-    middlewares(){
+    middlewares() {
         //CORS
         this.app.use(cors());
         //Cualqueir informacion que venga va ser serializada a formato JSON
@@ -20,16 +28,16 @@ class Servidor {
         this.app.use(express.static('public'));
     }
     //Crear el metodo routes donde estaran las rutas
-    routes(){
-      this.app.use('/api/usuarios',require('../routes/usuarios'));
+    routes() {
+        this.app.use('/api/usuarios', require('../routes/usuarios'));
     }
 
     //Crear el metodo donde el servidor esta escuhando el puerto
 
-    listen(){
-        this.app.listen(this.port,()=>{
-            console.log('Servidor ejecutandose en el puerto',this.port)
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Servidor ejecutandose en el puerto', this.port)
         })
     }
 }
-module.exports=Servidor;
+module.exports = Servidor;
